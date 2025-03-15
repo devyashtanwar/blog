4c162f772e3d6dde910296a8cd002df51bf6bc0e
Devyash Tanwar <tanwardevyash@gmail.com>
2025-03-16 01:37:03 +0530

    added the footer

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 'use client'
 import Header from "./components/header";
 import BlogList from "./components/blog-list";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
       <>
         <Header />
         <BlogList />
+        <Footer />
       </>
   );
 }
