41f6c97a8531f169f22d78b8d657973f2b44b1d1
Devyash Tanwar <tanwardevyash@gmail.com>
2025-03-16 00:24:13 +0530

    created the blog post interface for homepage

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Header from "./components/header";
+import BlogItem from "./components/blog-item.jsx";
 
 export default function Home() {
   return (
       <>
         <Header />
+        <BlogItem />
       </>
   );
 }
