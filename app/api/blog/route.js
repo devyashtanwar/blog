ec5fa8fcb2aaac160146f2f35b2d4e77b9c89f99
Devyash Tanwar <tanwardevyash@gmail.com>
2025-03-16 15:30:22 +0530

    created api to add blog to the database

---
+++ app/api/blog/route.js
@@ -0,0 +1,41 @@
+import { ConnectDB } from "@/lib/config/db"
+import { NextResponse } from "next/server";
+import { writeFile } from "fs/promises"
+import BlogModel from "@/lib/models/blog-model";
+
+const LoadDB = async () => {
+  await ConnectDB();
+}
+
+LoadDB();
+
+export const GET = async (request) => {
+
+  return NextResponse.json ({msg: "Api working"})
+}
+
+export const POST = async (request) => {
+  const formData = await request.formData();
+  const timestamp = Date.now();
+
+  const image = formData.get('image');
+  const imageByteData = await image.arrayBuffer();
+  const buffer = Buffer.from(imageByteData);
+  const path = `./public/${timestamp}_${image.name}`;
+  await writeFile(path, buffer);
+  const imgUrl = `/${timestamp}_${image.name}`;
+
+  const blogData = {
+    title: `${formData.get('title')}`,
+    description: `${formData.get('description')}`,
+    category: `${formData.get('category')}`,
+    author: `${formData.get('author')}`,
+    image: `${imgUrl}`,
+    authorImg: `${formData.get('authorImg')}`
+  }
+
+  await BlogModel.create(blogData);
+  console.log("Blog Saved!");
+
+  return NextResponse.json({ success : true, msg: "Blog added successfully" });
+}
