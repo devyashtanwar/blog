f3c85bd848c30fd7f7e0b9e9aeae1c6ce9d3a9e3
Devyash Tanwar <tanwardevyash@gmail.com>
2025-03-17 20:35:11 +0530

    added get api endpoint and prettier formatted the file

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,24 +1,26 @@
-import { ConnectDB } from "@/lib/config/db"
+import { ConnectDB } from "@/lib/config/db";
 import { NextResponse } from "next/server";
-import { writeFile } from "fs/promises"
+import { writeFile } from "fs/promises";
 import BlogModel from "@/lib/models/blog-model";
 
 const LoadDB = async () => {
   await ConnectDB();
-}
+};
 
 LoadDB();
 
+//API endpoint to get all blogs
 export const GET = async (request) => {
+  const blogs = await BlogModel.find({});
+  return NextResponse.json({ blogs });
+};
 
-  return NextResponse.json ({msg: "Api working"})
-}
-
+// API endpoint for uploading new blogs
 export const POST = async (request) => {
   const formData = await request.formData();
   const timestamp = Date.now();
 
-  const image = formData.get('image');
+  const image = formData.get("image");
   const imageByteData = await image.arrayBuffer();
   const buffer = Buffer.from(imageByteData);
   const path = `./public/${timestamp}_${image.name}`;
@@ -26,16 +28,16 @@ export const POST = async (request) => {
   const imgUrl = `/${timestamp}_${image.name}`;
 
   const blogData = {
-    title: `${formData.get('title')}`,
-    description: `${formData.get('description')}`,
-    category: `${formData.get('category')}`,
-    author: `${formData.get('author')}`,
+    title: `${formData.get("title")}`,
+    description: `${formData.get("description")}`,
+    category: `${formData.get("category")}`,
+    author: `${formData.get("author")}`,
     image: `${imgUrl}`,
-    authorImg: `${formData.get('authorImg')}`
-  }
+    authorImg: `${formData.get("authorImg")}`,
+  };
 
   await BlogModel.create(blogData);
   console.log("Blog Saved!");
 
-  return NextResponse.json({ success : true, msg: "Blog added successfully" });
-}
+  return NextResponse.json({ success: true, msg: "Blog added successfully" });
+};
