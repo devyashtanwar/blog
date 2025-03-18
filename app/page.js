f5ea9e4de4af91bf08b4eba9b02aebd628901646
Devyash Tanwar <tanwardevyash@gmail.com>
2025-03-18 22:52:59 +0530

    added toastify container to be used in the homepage

--- app/page.js
+++ app/page.js
@@ -1,14 +1,17 @@
-'use client'
+"use client";
 import Header from "./components/header";
 import BlogList from "./components/blog-list";
 import Footer from "./components/footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
   return (
-      <>
-        <Header />
-        <BlogList />
-        <Footer />
-      </>
+    <>
+      <ToastContainer theme="dark" />
+      <Header />
+      <BlogList />
+      <Footer />
+    </>
   );
 }
